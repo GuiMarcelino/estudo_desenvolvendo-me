c7e4cff8f2f0e3a5043fea868632329b3b529846
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-03-11 06:44:52 -0300

    exercicio contar letras numeros e caracteres

--- geeksforgeeks_1.rb
+++ geeksforgeeks_1.rb
@@ -5,9 +5,16 @@ com_num = a.size
 sem_num = a.tr("0-9", "").size
 
 sem_caracter = a.gsub(/[^a-zA-Z\s.]/,'').size
+maiusculas = a.scan(/\p{Upper}/).size
+minusculas = a.scan(/\p{Lower}/).size
 
+numeros = com_num.to_i - sem_num.to_i
+caracteres = sem_num.to_i - sem_caracter.to_i
 
+puts "Letras maiúsculas: #{maiusculas}"
 
-puts "Números: #{com_num.to_i - sem_num.to_i}"
+puts "Letras minúsculas: #{minusculas}"
 
-puts "Caracteres especiais: #{sem_num.to_i - sem_caracter.to_i}"
+puts "Números: #{numeros}"
+
+puts "Caracteres especiais: #{caracteres}"
