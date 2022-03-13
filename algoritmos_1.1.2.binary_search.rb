c33c643c3eda0fe493075466ed8b96b7fbbc82d0
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-03-13 18:17:57 -0300

    exercicio binaria array

--- algoritmos_1.1.2.binary_search.rb
+++ algoritmos_1.1.2.binary_search.rb
@@ -1,12 +1,32 @@
 # Problema: Dado um
 # array ordenado arr[] de n elementos, escreva uma função para buscar um dado elemento x em arr[].
 
-def search(array, last, r, x)
 
 
+
+def search(array, x)
+  # return puts "Valor não existe no array" unless array.include?(x) # só continua se o valor existir no array
+
+  array.find_index(x) # indice do valor
+
+  half = array.sort!.length / 2 # metade do indice
+  middle = array.length.odd? ? array[half] : (array[half] + array[half - 1]) / 2 # valor que pode estar no indice do meio do array
+
+  if middle == x   # está no meio do array ?
+    puts "O elemento #{x} está presente no índice #{array.find_index(x)}"
+
+  elsif array[0..half].include?(x) # está na primera metade do array ?
+    puts "O elemento #{x} está presente no índice #{array.find_index(x)}"
+
+  elsif array[half..-1].include?(x) # está na segunda metade do array?
+    puts "O elemento #{x} está presente no índice #{array.find_index(x)}"
+  else
+     puts "O elemento #{x} não está presente no array[]."
+  end
 end
 
 array = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
-last = array[-1] # || array.last
-first = array[0] # || array.first
-x = 60
+
+puts "Digite um número para realizar a busca: "
+x = gets.to_i
+search(array, x)
