9bb8db33a9911339642f24a89c8651973322a78a
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-03-13 08:24:48 -0300

    exercicio binaria array

---
+++ algoritmos_1.1.2.binary_search.rb
@@ -0,0 +1,12 @@
+# Problema: Dado um
+# array ordenado arr[] de n elementos, escreva uma função para buscar um dado elemento x em arr[].
+
+def search(array, last, r, x)
+
+
+end
+
+array = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
+last = array[-1] # || array.last
+first = array[0] # || array.first
+x = 60
