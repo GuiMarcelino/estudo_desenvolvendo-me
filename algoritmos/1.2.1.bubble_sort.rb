d384f995490a66be59545ef914fc0f2d77204d88
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-03-15 06:04:31 -0300

    exericio bubble_sort

--- algoritmos/1.2.1.bubble_sort.rb
+++ algoritmos/1.2.1.bubble_sort.rb
@@ -1,13 +1,26 @@
 # Bubble Sort é o algoritmo de ordenação mais simples que funciona trocando repetidamente os elementos adjacentes se estiverem na ordem errada.
+require 'byebug'
 
 def bubble_sort(array)
-  seq = array.sort
 
-  if seq[0..1] == array[0..1]
 
+  array.length
+  count = 0
+
+  while array.length > count
+
+    array_sort = []
+    count +=1
+
+    if array[0] > array[1]
+
+      array_sort << array[1]
+      array_sort << array[0]
+
+    end
   end
-
 end
 
 array = [64, 34, 25, 12, 22, 11, 90]
 
+bubble_sort(array)
