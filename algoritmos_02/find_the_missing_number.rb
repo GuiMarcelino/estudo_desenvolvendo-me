5a366194c59734b9fc458d3b08f370388c6fedec
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-27 18:28:42 -0300

    testes algoritmos_02

--- algoritmos_02/find_the_missing_number.rb
+++ algoritmos_02/find_the_missing_number.rb
@@ -8,20 +8,14 @@ Entrada: arr[] = {1, 2, 4, 6, 3, 7, 8}
  Saída: 4
  Explicação: O número ausente de 1 a 5 é 4
 "
-def missng_number(array)
 
-  range = array[0]..array[-1]
+def missing_number(array)
 
-  first_position = array[0]
-  last_position = array[-1]
-  mid = 0
-
-  while last_position > first_position + 1
-    mid_array = (first_position + last_position) / 2
-    if (first_position - last_position) != ()
-  end
-
-
+  qtd_elements = array.length # quantidade de número no array
+  total = (qtd_elements + 1)*(qtd_elements + 2)/2
+  sum_of_array = array.sum # soma todos os elementos do array
+  total - sum_of_array # faz o cálculo que retorna o número que falta
 end
-array = [1, 2, 4, 6, 3, 7, 8]
-missng_number(array)
+
+array = [6,7,9,10]
+missing_number(array)
